993fcefe2b0d44c041107fdb9a5274b9c37aadee
andysizer <andy.sizer@enclaveit.com>
2011-08-01 16:54:42 +0700

    random changes to support both sites.

--- src/gameConfig.hs
+++ src/gameConfig.hs
@@ -9,7 +9,6 @@ module GameConfig
 )
     where
 
-{--
 userDataPath = "D:\\Projects\\wesnoth\\wesnoth-1.8.6\\userdata\\"
 
 systemDataPath = "d:\\Projects\\wesnoth\\wesnoth-1.8.6\\data\\"
@@ -17,8 +16,8 @@ systemDataPath = "d:\\Projects\\wesnoth\\wesnoth-1.8.6\\data\\"
 gameConfigPath = "d:\\Projects\\wesnoth\\wesnoth-1.8.6\\"
 
 wesnothProgDir = "d:\\Projects\\wesnoth\\wesnoth-1.8.6\\"
---}
 
+{--
 userDataPath = "C:\\Users\\andy\\Documents\\My Games\\Wesnoth1.8\\"
 
 systemDataPath = "C:\\Program Files\\Battle for Wesnoth 1.8.6\\data"
@@ -26,3 +25,4 @@ systemDataPath = "C:\\Program Files\\Battle for Wesnoth 1.8.6\\data"
 gameConfigPath = "C:\\Program Files\\Battle for Wesnoth 1.8.6\\"
 
 wesnothProgDir = "C:\\Program Files\\Battle for Wesnoth 1.8.6\\"
+--}
--- src/wesConfig.hs
+++ src/wesConfig.hs
@@ -2,11 +2,18 @@
 
 module WesConfig
 (
-  version
+  localeDir
+, version
 , package
+, minSaveGameVersion
 )
-
     where
 
+localeDir = "translations"
+
 version = "1.8.6"
+
 package = "wesnoth"
+
+minSaveGameVersion = "1.3.10"
+
