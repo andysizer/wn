7adab42d4ff9d2f1c0707c1bdfd8a641ab89402c
unknown <andy@.(none)>
2011-08-06 09:53:46 +0700

    parser harness

---
+++ src/pw.hs
@@ -0,0 +1,26 @@
+-- file: pw.hs
+
+import System.Environment
+import System.Exit
+import ApplicativeParsec
+import PreProcessWmlIO
+import ParseWml
+import Utf8ReadWriteFile
+
+main = do
+    args <- getArgs
+    doIt args
+
+doIt [i] = pw i o
+    where o = "pw.parse"
+doIt [i,o] = pw i o
+doIt _ = do
+    usage
+    exitFailure
+
+usage = do
+    p <- getProgName
+    print ("Usage: " ++ p ++ " input [outfile]")
+
+pw x y = do { p <- preProcessWmlFile x; c <- parseWml p; writeFileUtf8 y c;}
+    
